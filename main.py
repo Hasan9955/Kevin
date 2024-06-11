392cc51e800ba7effe2aaeeb551f31799172e659
Hasan Bin Ali <hasanbinali7556@gmail.com>
2024-06-11 17:09:41 +0600

    Fix some error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import speech_recognition as sr;
+import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
@@ -6,77 +6,93 @@ import wikipedia
 import pyjokes
 
 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
 
-# change the voice male to female
+
+# Initialize text-to-speech engine
+engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
+listener = sr.Recognizer()
 
 def talk(text):
-    # engine.say('Hi, I am kevin')
     engine.say(text)
     engine.runAndWait()
 
-def talk_command():
-    try:  
+def take_command(timeout = 5, phrase_time_limit=5):
+    command = ""
+    try:
         with sr.Microphone() as source:
-            print("listening.....")
-            voice = listener.listen(source)
+            print("Listening...")
+            listener.adjust_for_ambient_noise(source)
+            voice = listener.listen(source, timeout = timeout, phrase_time_limit=phrase_time_limit)
             command = listener.recognize_google(voice)
             command = command.lower()
+            print(command)
             if 'kevin' in command:
-                command = command.replace('kevin', '')
-                print(command)
-
-    except:
-        pass
+                command = command.replace('kevin', '') 
+    
+    except sr.UnknownValueError:
+        print("Sorry, I did not understand that")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
     return command
 
 
+
 def run_kevin():
-    command = talk_command()
-    print(command)
-    if 'play' in command: 
-        song = command.replace('play', '')
-        talk('playing' + song)
-        pywhatkit.playonyt(song)
-    elif 'how are you' in command: 
-        print("I am good, thank you for asking. I hope you're doing well too. If I can help with anything, just  ask.")
-        talk("I am good, thank you for asking. I hope you're doing well too. If I can help with anything, just  ask.")
-
-    
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        print(time)
-        talk('Current time is' + time)
-    
-    elif 'date' in command:
-        date = datetime.datetime.now().date()
-        print(date)
-        talk(date)
-
-    elif 'tell me about' in command: 
-        person = command.replace('tell me about', '')
-        info = wikipedia.summary(person, 3)
-        print(info)
-        talk(info)
-    
-    elif 'what is your name' in command:
-        print('My name is kevin. My mission is To assist you.')
-        talk('My name is kevin. My mission is To assist you.')
-
-    elif 'are you single' in command: 
-        talk('I am in a relationship with wifi')
-        
-    elif 'jokes' in command:
-        jokes = pyjokes.get_joke()
-        print(jokes)
-        talk(jokes)
-    else: 
-        talk('I did not get it but I am going to search it for you')
-        pywhatkit.search(command)
+    command = take_command()
+    if command:
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk(f'Playing {song}')
+            pywhatkit.playonyt(song)
+        elif 'how are you' in command:
+            response = "I am good, thank you for asking. I hope you're doing well too. If I can help with anything, just ask."
+            print(response)
+            talk(response)
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%I:%M %p')
+            print(time)
+            talk(f'Current time is {time}')
+        elif 'date' in command:
+            date = datetime.datetime.now().strftime('%Y-%m-%d')
+            print(date)
+            talk(f'Today\'s date is {date}')
+        elif 'tell me about' in command:
+            person = command.replace('tell me about', '')
+            try:
+                info = wikipedia.summary(person, sentences=3)
+                print(info)
+                talk(info)
+            except wikipedia.exceptions.DisambiguationError as e:
+                print(f"Disambiguation error: {str(e)}")
+                talk("There are multiple results for this topic, please be more specific.")
+            except wikipedia.exceptions.PageError:
+                print("Page not found")
+                talk("I couldn't find any information on that topic.")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+                talk("Sorry, I couldn't retrieve the information.")
+        elif 'what is your name' in command:
+            response = 'My name is Kevin. My mission is to assist you.'
+            print(response)
+            talk(response)
+        elif 'are you single' in command:
+            response = 'I am in a relationship with Wi-Fi.'
+            print(response)
+            talk(response)
+        elif 'joke' in command:
+            joke = pyjokes.get_joke()
+            print(joke)
+            talk(joke)
+        else:
+            error_message = "I did not get that, but I am going  to search it for you!"
+            print(error_message)
+            talk(error_message)
+            pywhatkit.search(command)
+    else:
+        print("No command received.")
 
 while True:
     run_kevin()
